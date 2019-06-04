709f2e6bfc746e416cea5361019a20208258fe48
Mingzhi Zheng <mizheng@microsoft.com>
2019-06-03 23:15:34 -0700

    remove dummy data util & update data/pipeline with dummy implementation

--- graph_network/data/pipeline.py
+++ graph_network/data/pipeline.py
@@ -8,20 +8,7 @@ class DataPipline(object):
     def __init__(self,
                  dataset,
                  batch_size,
-                 enable_shuffle,
-                 buffer_size=10000,
+                 enable_shuffle=True,
                  random_seed=0):
         """initialize data pipeline"""
-        if enable_shuffle == True:
-            dataset = dataset.shuffle(buffer_size, random_seed)
-        
-        dataset = dataset.batch(batch_size=batch_size)
-        dataset = dataset.prefetch(buffer_size=1)
-        
-        iterator = dataset.make_initializable_iterator()
-        self.batch_data = iterator.get_next()
-        self.initializer = iterator.initializer
-        
-        self.dataset = dataset
-        self.batch_size = batch_size
-        self.enable_shuffle = enable_shuffle
+        pass
