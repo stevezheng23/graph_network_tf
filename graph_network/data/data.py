c72c72ea71ac39f8d6fee25f9352cccde7ef4a2e
Mingzhi Zheng <mizheng@microsoft.com>
2019-06-04 21:48:00 -0700

    add default key access for graph data

--- graph_network/data/data.py
+++ graph_network/data/data.py
@@ -10,8 +10,31 @@ class GraphData(object):
                  node_attr,
                  edge_attr,
                  graph_attr,
-                 node_label,
-                 edge_label,
-                 graph_label):
+                 node_label=None,
+                 edge_label=None,
+                 graph_label=None):
         """initialize graph data"""
-        pass
+        self.edge_list = edge_list
+        self.node_attr = node_attr
+        self.edge_attr = edge_attr
+        self.graph_attr = graph_attr
+        self.node_label = node_label
+        self.edge_label = edge_label
+        self.graph_label = graph_label
+    
+    def __getitem__(self,
+                    key):
+        return getattr(self, key)
+
+    def __setitem__(self,
+                    key,
+                    value):
+        setattr(self, key, value)
+    
+    @property
+    def keys(self):
+        return [key for key in self.__dict__.keys() if self[key] is not None]
+    
+    def __contains__(self,
+                     key):
+        return key in self.keys
