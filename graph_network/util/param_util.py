eadd45e523e61dbc69d05e11277d32fa3b5b9185
Mingzhi Zheng <mizheng@microsoft.com>
2019-06-07 13:37:45 -0700

    add missing log path in param util & default config

--- graph_network/util/param_util.py
+++ graph_network/util/param_util.py
@@ -14,6 +14,7 @@ def create_default_hyperparams(config_type):
     """create default hyperparameters"""
     if config_type == "default":
         hyperparams = tf.contrib.training.HParams(
+            data_log_output_dir="",
             model_type="default",
             model_scope="graph",
             device_num_gpus=1,
