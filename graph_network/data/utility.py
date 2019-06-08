25a759d78755b5d84c8b14aeda2d03e2e8615279
Mingzhi Zheng <mizheng@microsoft.com>
2019-06-07 18:43:52 -0700

    add data reader for processing in keneral dataset

--- graph_network/data/utility.py
+++ graph_network/data/utility.py
@@ -1,9 +1,10 @@
 import os.path
 import urllib.request
+import shutil
 import tarfile
 import zipfile
 
-__all__ = ["download_from_url", "extract_zip", "extract_tar"]
+__all__ = ["download_from_url", "extract_zip", "extract_tar", "valid_path", "clear_path", "read_file"]
 
 def download_from_url(url,
                       path):
@@ -21,3 +22,16 @@ def extract_tar(tar_path,
                 mode='r:gz'):
     with tarfile.open(tar_path, mode) as file:
         file.extractall(file_path)
+
+def valid_path(path):
+    if not os.path.exists(path):
+        os.mkdir(path)
+
+def clear_path(path):
+    if os.path.exists(path):
+        shutil.rmtree(path)
+
+def read_file(path):
+    with open(path) as file:
+        data_list = [line.rstrip('\n') for line in file]
+        return data_list
