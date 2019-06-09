d4f4bc2a3311c427a015792b2ffa9d63fc160996
Mingzhi Zheng <mizheng@microsoft.com>
2019-06-09 10:46:10 -0700

    add read/save files support in data.utility

--- graph_network/data/utility.py
+++ graph_network/data/utility.py
@@ -1,15 +1,17 @@
 import os.path
+import json
 import urllib.request
 import shutil
 import tarfile
 import zipfile
 
-__all__ = ["download_from_url", "extract_zip", "extract_tar", "valid_path", "clear_path", "read_file"]
+__all__ = ["download_from_url", "extract_zip", "extract_tar",
+           "valid_path", "clear_path", "read_text", "read_json", "save_text", "save_json"]
 
-def download_from_url(url,
-                      path):
-    data = urllib.request.urlopen(url)
-    with open(path, 'wb') as file:
+def download_from_url(data_url,
+                      data_path):
+    data = urllib.request.urlopen(data_url)
+    with open(data_path, 'wb') as file:
         file.write(data.read())
 
 def extract_zip(zip_path,
@@ -23,15 +25,43 @@ def extract_tar(tar_path,
     with tarfile.open(tar_path, mode) as file:
         file.extractall(file_path)
 
-def valid_path(path):
-    if not os.path.exists(path):
-        os.mkdir(path)
+def valid_path(data_path):
+    if not os.path.exists(data_path):
+        os.mkdir(data_path)
 
-def clear_path(path):
-    if os.path.exists(path):
-        shutil.rmtree(path)
+def clear_path(data_path):
+    if os.path.exists(data_path):
+        shutil.rmtree(data_path)
 
-def read_file(path):
-    with open(path) as file:
-        data_list = [line.rstrip('\n') for line in file]
-        return data_list
+def read_text(data_path):
+    if os.path.exists(data_path):
+        with open(data_path, "r") as file:
+            return [line.rstrip('\n') for line in file]
+    else:
+        raise FileNotFoundError("input file not found")
+
+def read_json(data_path):
+    if os.path.exists(data_path):
+        with open(data_path, "r") as file:
+            return json.load(file)
+    else:
+        raise FileNotFoundError("input file not found")
+
+def save_text(data_list,
+              data_path):
+    data_folder = os.path.dirname(data_path)
+    if not os.path.exists(data_folder):
+        os.mkdir(data_folder)
+    
+    with open(data_path, "w") as file:
+        for data in data_list:
+            file.write("{0}\n".format(data))
+
+def save_json(data_list,
+              data_path):
+    data_folder = os.path.dirname(data_path)
+    if not os.path.exists(data_folder):
+        os.mkdir(data_folder)
+    
+    with open(data_path, "w") as file:  
+        json.dump(data_list, file, indent=4)
